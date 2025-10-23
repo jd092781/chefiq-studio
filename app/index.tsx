d6357afa3e62fef90da61717708902532400dc78
Jeff <jdypko@gmail.com>
2025-10-23 12:36:26 -0400

    Web: force entry to /(tabs)/home

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,5 @@
 // app/index.tsx
 import { Redirect } from "expo-router";
-
 export default function Index() {
-  // Send web users straight into the tabs navigator at Home
   return <Redirect href="/(tabs)/home" />;
 }
